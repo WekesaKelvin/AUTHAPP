0cbc88960d6fb82f265e5f788ee671f749cbc44c
Kelvin Wekesa <kelvin.wekesa@Kelvins-MacBook-Pro-M4.local>
2025-03-29 13:03:34 +0300

    Prepare for Jest configuration

--- jest.config.js
+++ jest.config.js
@@ -5,9 +5,6 @@ module.exports = {
   transform: {
     '^.+\\.(ts|js|html)$': 'ts-jest',
   },
-  moduleNameMapper: {
-    '^@app/(.*)$': '<rootDir>/src/app/$1',
-    '^@environments/(.*)$': '<rootDir>/src/environments/$1',
-  },
-  testMatch: ['**/__tests__/**/*.spec.ts', '**/*.spec.ts'],
+  moduleFileExtensions: ['ts', 'html', 'js', 'json'],
+  testMatch: ['**/+(*.)+(spec).+(ts|js)?(x)']
 };
